c7c9ea5159c4928d42079bb49c66e9048c2b81bb
makejavas <1353036300@qq.com>
2017-05-02 15:50:09 +0800

    测试提交

--- src/main/src/com/sjhy/controller/TestController.java
+++ src/main/src/com/sjhy/controller/TestController.java
@@ -3,10 +3,11 @@ package com.sjhy.controller;
 import com.jfinal.core.Controller;
 
 /**
- * Created by Administrator on 2017/5/2 0002.
+ * Created by wjh on 2017/5/2 0002.
  */
 public class TestController extends Controller {
     public void index(){
+        System.out.println("尝试修改并提交一次！");
         render("index.vm");
     }
 }
